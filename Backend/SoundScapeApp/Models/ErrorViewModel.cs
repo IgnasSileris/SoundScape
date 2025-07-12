55f4de32849b7888a4b3ead6121877220fb7178e
Ignas Sileris <ignasileris@gmail.com>
2025-07-12 13:57:28 +0100

    Electron backend initialisation

---
+++ Backend/SoundScapeApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SoundScapeApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
