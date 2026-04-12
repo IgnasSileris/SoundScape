afdf4d75a32b553593bbcdc32e884de35650bef9
Ignas Sileris <ignasileris@gmail.com>
2026-04-12 18:51:05 +0100

    Change circular buffer implementation to 1) use generics, 2) do overwriting and 3) accomodate BulkReads and Writes

--- Backend/SoundScapeApp/Services/CircularBuffer.cs
+++ Backend/SoundScapeApp/Services/CircularBuffer.cs
@@ -1,37 +1,101 @@
+using System;
+
 namespace SoundScapeApp.Services;
 
-public class CircularBuffer
+/// <summary>
+/// Circular Buffer implementation with overwrite.
+/// </summary>
+public class CircularBuffer<T>(int _capacity)
 {
-    private const int Capacity = 10;
-    private readonly byte[] _buffer = new byte[Capacity];
-    private int _writeIndex = 0;
-    private int _readIndex = 0;
+    private readonly int capacity = _capacity;
+    private readonly T[] buffer = new T[_capacity];
+    private int writeIndex = 0;
+    private int readIndex = 0;
+    private int count = 0;
 
-    public bool Write(byte byteToWrite)
+
+    public bool Write(T dataToWrite)
     {
-        if ((_writeIndex + 1) % Capacity == _readIndex)
+        if (count == capacity)
         {
-            // buffer is full, skip
-            return false;
+            // buffer full (fully unread)
+            readIndex = (readIndex + 1) % capacity;
+        }
+        else
+        {
+            count++;
         }
 
-        _buffer[_writeIndex] = byteToWrite;
-        _writeIndex = (_writeIndex + 1) % Capacity;
+        buffer[writeIndex] = dataToWrite;
+        writeIndex = (writeIndex + 1) % capacity;
 
         return true;
     }
 
-    public bool Read(ref byte byteToReadTo)
+    public int BulkWrite(ReadOnlySpan<T> bulkDataToWrite)
     {
-        if (_readIndex == _writeIndex)
+        int startIndex = 0;
+        int toWrite = bulkDataToWrite.Length;
+        if (toWrite > capacity)
+        {
+            // only take the latest data
+            toWrite = capacity;
+            startIndex = bulkDataToWrite.Length - capacity;
+        }
+
+        // write up to the end of the buffer
+        int firstPart = Math.Min(toWrite, capacity - writeIndex);
+
+        bulkDataToWrite[startIndex..(startIndex + firstPart)].CopyTo(buffer.AsSpan(writeIndex, firstPart));
+
+        if (firstPart < toWrite)
+        {
+            int secondPart = toWrite - firstPart;
+            bulkDataToWrite[(startIndex + firstPart)..(startIndex + firstPart + secondPart)].CopyTo(buffer.AsSpan(0, secondPart));
+        }
+
+        int overwrites = Math.Max(0, count + toWrite - capacity);
+        readIndex = (readIndex + overwrites) % capacity;
+
+        writeIndex = (writeIndex + toWrite) % capacity;
+        count = Math.Min(count + toWrite, capacity);
+
+        return toWrite;
+    }
+
+    public bool Read(out T dataToReadTo)
+    {
+        if (count == 0)
         {
             // buffer is empty
+            dataToReadTo = default!;
             return false;
         }
 
-        byteToReadTo = _buffer[_readIndex];
-        _readIndex = (_readIndex + 1) % Capacity;
+        dataToReadTo = buffer[readIndex];
+        readIndex = (readIndex + 1) % capacity;
+        count--;
 
         return true;
+    }
+
+    public int BulkRead(Span<T> bulkDataToWriteTo)
+    {
+        int toRead = Math.Min(bulkDataToWriteTo.Length, count);
+
+        int firstPart = Math.Min(capacity - readIndex, toRead);
+
+        buffer.AsSpan(readIndex, firstPart).CopyTo(bulkDataToWriteTo[..firstPart]);
+
+        if (firstPart < toRead)
+        {
+            int secondPart = toRead - firstPart;
+            buffer.AsSpan(firstPart, secondPart).CopyTo(bulkDataToWriteTo[firstPart..secondPart]);
+        }
+
+        readIndex = (readIndex + toRead) % capacity;
+        count = Math.Max(0, count - toRead);
+
+        return toRead;
     }
 }
