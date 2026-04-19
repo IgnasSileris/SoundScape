824142653bd6377cdb0c1e468d33406109920b28
Ignas Sileris <ignasileris@gmail.com>
2026-04-19 14:11:52 +0100

    initial AudioManager

--- Backend/SoundScapeApp/Services/AudioManager.cs
+++ Backend/SoundScapeApp/Services/AudioManager.cs
@@ -1,4 +1,58 @@
-public class AudioManager
+using System;
+using Microsoft.Extensions.Hosting;
+using SoundScapeApp.Libraries.Filters;
+
+namespace SoundScapeApp.Services;
+
+public class AudioManager(AudioStateService _state, CircularBuffer<float> _inputBuffer, CircularBuffer<float> _outputBuffer, FilterRegistry _filterRegistry) : BackgroundService
 {
-    
+    private readonly AudioStateService state = _state;
+    private readonly CircularBuffer<float> inputBuffer = _inputBuffer;
+    private readonly CircularBuffer<float> outputBuffer = _outputBuffer;
+    private readonly FilterRegistry filterRegistry = _filterRegistry;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        state.OnActiveFiltersChanged += OnActiveFiltersChangedHandler;
+
+        try
+        {
+            Span<float> rawAudioChunk = new float[32];
+
+            int readBytes = inputBuffer.BulkRead(rawAudioChunk);
+            var processedAudio = ApplyFilters(rawAudioChunk);
+            int writtenBytes = outputBuffer.BulkWrite(processedAudio);
+
+            await Task.Delay(Timeout.Infinite, stoppingToken);
+        }
+        finally
+        {
+            state.OnActiveFiltersChanged -= OnActiveFiltersChangedHandler;
+        }
+    }
+
+    private Span<float> ApplyFilters(Span<float> rawAudioChunk)
+    {
+        foreach (string id in state.ActiveFilterIds)
+        {
+            var filter = filterRegistry.GetFilter(id);
+
+            if (filter == null)
+            {
+                continue;
+            }
+
+            // TODO: call filter function here
+        }
+
+        return rawAudioChunk;
+    }
+
+
+    private void OnActiveFiltersChangedHandler(List<string> activeFilterIds)
+    {
+        return;
+    }
+
+
 }
